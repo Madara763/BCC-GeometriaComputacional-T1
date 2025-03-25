3af66ecbcb4724a9a208ae8131aa63c66698a375
Madara763 <clashroyalemadara@gmail.com>
2025-03-25 16:49:22 -0300

    inicio

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+  //Leitura dos dados
+  int m,n; //quantidade de poligonos e pontos respectivamente
+  
+  cin>>m>>n;
+
+  //Aloca a estrutura para os poligonos
+  vector<vector<pair<int64_t, int64_t>>>; //estou pensando sobre como fazer isso da melhor forma
+
+  //le os vertices dos m poligonos
+  int v;
+  for(int i=0; i<m; i++){
+    
+    cin>>v; //le quantos vertices 
+    for(int j=0; j<v; j++){ //Le as coordenadas de cada vertice
+
+    }//for j
+  }//for i
+
+
+  return 0;
+}
