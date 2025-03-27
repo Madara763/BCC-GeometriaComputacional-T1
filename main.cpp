d7fc35fe167cbd6dc123c951a2f380750fae8495
Madara763 <clashroyalemadara763@gmail.com>
2025-03-27 11:42:15 -0300

    inicio

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <list>
 
 using namespace std;
+#define xx first
+#define yy second
 
 int main(){
 
@@ -12,16 +15,25 @@ int main(){
   cin>>m>>n;
 
   //Aloca a estrutura para os poligonos
-  vector<vector<pair<int64_t, int64_t>>>; //estou pensando sobre como fazer isso da melhor forma
+  //Lista para um vetor de pontos (vertices)
+  //[] -> {(x,y), (x,y), (x,y)}
+
+  list<vector<pair<int64_t, int64_t>>> lista{}; //estou pensando sobre como fazer isso da melhor forma
 
   //le os vertices dos m poligonos
   int v;
+  int64_t x,y;
   for(int i=0; i<m; i++){
     
     cin>>v; //le quantos vertices 
-    for(int j=0; j<v; j++){ //Le as coordenadas de cada vertice
+    
+    vector<pair<int64_t, int64_t>> vetor(v); //Cria o vetor que armazenara a seq de pontos
 
+    for(int j=0; j<v; j++){ //Le as coordenadas de cada vertice
+      cin>>vetor[j].xx>>vetor[j].yy;
     }//for j
+    lista.insert
+    
   }//for i
 
 
